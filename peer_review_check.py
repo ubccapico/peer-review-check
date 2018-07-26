4095b45fa27236d5aed610b2727fa50ca7f39c49
Barish Golland <barish.golland@sauder.ubc.ca>
2018-07-26 14:31:31 -0700

    Add files via upload

--- peer_review_check.py
+++ peer_review_check.py
@@ -20,6 +20,7 @@ except:
     pip.main(['install', 'json'])
     import json
 
+#Ensure Canvas API token is in the designated file Canvas API Token.txt
 print ('Before you begin the process, please ensure you have copy & pasted your Canvas API token into the file Canvas API Token.txt.')
 confirmation = input ('Input any key to continue:')
 
@@ -28,22 +29,22 @@ with open('Canvas API Token.txt','r') as f:
         for word in line.split():
            token = word   
 
-#course url
+#Course url
 url = "https://ubc.instructure.com/"
 
-#course number
+#Course number
 course = input('Input course ID and hit ENTER:\n')
 
-#the assignment ID number
+#The assignment ID number
 assignment_id = input('Assignment ID number and hit ENTER:\n')
 
 print ('Processing data, please wait......\n')
-#collecting data: extracts peer review information for the assignment, reviews list of peer reviews
-#and finds the total amount of peer reviews (maximum 3) each student has completed 
+
 try:
+#Obtaining the assignment information (settings, assignment id, rubric id)
     assignmentInfo = requests.get(url + '/api/v1/courses/' + str(course) + '/assignments/' + assignment_id,
                  headers= {'Authorization': 'Bearer ' + token})
-
+#Extracting assignment rubric id/rubric for the assignment
     assignmentInfo = json.loads(assignmentInfo.text)
 
     rubric_id = str(assignmentInfo['rubric_settings']['id'])
@@ -55,9 +56,10 @@ try:
                  headers= {'Authorization': 'Bearer ' + token})
 
     rubric_return = json.loads(r.text)
+#Obtaining assessor_id (person who did peer review), score for the peer reviews
     assessments_df = pd.DataFrame(rubric_return['assessments'])
 
-#peer review information
+#Obtaining user_id (person who was peer reviewed), completion and submission comments
     peerReview = requests.get(url + '/api/v1/courses/' + str(course) + '/assignments/' + assignment_id + '/peer_reviews',
                  headers= {'Authorization': 'Bearer ' + token})
 
@@ -65,11 +67,12 @@ try:
     peerReview_df = pd.read_json(peerReview.text)
     peerReview_df['user_id'] = peerReview_df['user_id'].astype(str)
     
-
+#Merging data together into one csv file named 'peer review information.csv'
     merged_df = pd.merge(peerReview_df, assessments_df, how='outer', left_on=['assessor_id', 'asset_id'], right_on=['assessor_id', 'artifact_id'])
     merged_df.to_csv('peer_review_information.csv')
     print("peer review information file created")
 
+#Counts the number of peer reviews a student has completed, places into list with user_id (accessor_id)
     completed_peer_review_count = {}
     for index, row in merged_df.iterrows():
         assessor_id = str(row['assessor_id'])
@@ -81,18 +84,18 @@ try:
             
     
     print('Data successfully gathered.\n')
+#if input is True, uploads scores onto Canvas Gradecenter (does not create new assignment)
     upload = input ('Type True to upload peer review scores onto Gradecenter.\n')
     if upload==True:
     
         print("Data Should be Uploaded onto Gradecenter.")
-
         for assessor_id, peer_review_count in completed_peer_review_count.items():
             grade = 0
             if peer_review_count > 0:
                 grade = str(peer_review_count)
 
                 r = requests.put(
-                    url + '/api/v1/courses/' + str(course) + '/assignments/13302/submissions/' + str(assessor_id) + '/',
+                    url + '/api/v1/courses/' + str(course) + '/assignments/'+ str(assignment_id) +'/submissions/' + str(assessor_id) + '/',
                     params={'submission[posted_grade]': grade},
                     headers= {'Authorization': 'Bearer ' + token}
                     )
